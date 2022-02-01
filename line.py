bcf714503f22c79ea4de3fd6475b616ea6e25938
hara7610s <hara7610s@gmail.com>
2022-02-01 23:55:53 +0900

    revise usage of environment variable

--- line.py
+++ line.py
@@ -1,8 +1,6 @@
-import json
+import os
 import requests
-with open("line.json") as token_file:
-    line_json = json.load(token_file)
-LINE_NOTIFY_TOKEN = line_json["LINE_NOTIFY_TOKEN"]
+LINE_NOTIFY_TOKEN = os.environ["LINE_NOTIFY_TOKEN"]
 
 def notify_message(message):
     url = "https://notify-api.line.me/api/notify"
