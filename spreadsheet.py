51e5a606218db79bcc7f99c10f463fa2fa1e7f0b
hara7610s <hara7610s@gmail.com>
2022-02-09 20:07:56 +0900

    revise spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,16 +2,16 @@ import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-type = os.environ["TYPE"]
-project_id = os.environ["PROJECT_ID"]
-private_key_id = os.environ["PRIVATE_KEY_ID"]
-private_key = os.environ["PRIVATE_KEY"]
-client_email = os.environ["CLIENT_EMAIL"]
-client_id = os.environ["CLIENT_ID"]
-auth_uri = os.environ["AUTH_URI"]
-token_uri = os.environ["TOKEN_URI"]
-auth_provider_x509_cert_url = os.environ["AUTH_PROVIDER_X509_CERT_URL"]
-client_x509_cert_url = os.environ["CLIENT_X509_CERT_URL"]
+type = str(os.environ["TYPE"])
+project_id = str(os.environ["PROJECT_ID"])
+private_key_id = str(os.environ["PRIVATE_KEY_ID"])
+private_key = str(os.environ["PRIVATE_KEY"])
+client_email = str(os.environ["CLIENT_EMAIL"])
+client_id = str(os.environ["CLIENT_ID"])
+auth_uri = str(os.environ["AUTH_URI"])
+token_uri = str(os.environ["TOKEN_URI"])
+auth_provider_x509_cert_url = str(os.environ["AUTH_PROVIDER_X509_CERT_URL"])
+client_x509_cert_url = str(os.environ["CLIENT_X509_CERT_URL"])
 
 def read_spreadsheet():
     gcp_secret = {
