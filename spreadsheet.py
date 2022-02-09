16d3fe2931baad412c3476308a827f7a9244919d
hara7610s <hara7610s@gmail.com>
2022-02-09 19:59:45 +0900

    revise spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,18 +2,29 @@ import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+type = os.environ["TYPE"]
+project_id = os.environ["PROJECT_ID"]
+private_key_id = os.environ["PRIVATE_KEY_ID"]
+private_key = os.environ["PRIVATE_KEY"]
+client_email = os.environ["CLIENT_EMAIL"]
+client_id = os.environ["CLIENT_ID"]
+auth_uri = os.environ["AUTH_URI"]
+token_uri = os.environ["TOKEN_URI"]
+auth_provider_x509_cert_url = os.environ["AUTH_PROVIDER_X509_CERT_URL"]
+client_x509_cert_url = os.environ["CLIENT_X509_CERT_URL"]
+
 def read_spreadsheet():
     gcp_secret = {
-        'type':os.environ["TYPE"],
-        'project_id':os.environ["PROJECT_ID"],
-        'private_key_id':os.environ["PRIVATE_KEY_ID"],
-        'private_key':os.environ["PRIVATE_KEY"],
-        'client_email':os.environ["CLIENT_EMAIL"],
-        'client_id':os.environ["CLIENT_ID"],
-        'auth_uri':os.environ["AUTH_URI"],
-        'token_uri':os.environ["TOKEN_URI"],
-        'auth_provider_x509_cert_url':os.environ["AUTH_PROVIDER_X509_CERT_URL"],
-        'client_x509_cert_url':os.environ["CLIENT_X509_CERT_URL"]
+        'type':type,
+        'project_id':project_id,
+        'private_key_id':private_key_id,
+        'private_key':private_key,
+        'client_email':client_email,
+        'client_id':client_id,
+        'auth_uri':auth_uri,
+        'token_uri':token_uri,
+        'auth_provider_x509_cert_url':auth_provider_x509_cert_url,
+        'client_x509_cert_url':client_x509_cert_url
         }
 
     scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
