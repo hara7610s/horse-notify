67fa5b8d825d930fc484fe644b0d63229f6f5a24
hara7610s <hara7610s@gmail.com>
2022-02-09 20:22:40 +0900

    revise spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,3 +1,4 @@
+import gc
 import os
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -26,6 +27,8 @@ def read_spreadsheet():
         'auth_provider_x509_cert_url':auth_provider_x509_cert_url,
         'client_x509_cert_url':client_x509_cert_url
         }
+    
+    gcp_secret['private_key'] = gcp_secret['private_key'].replace('\\n', '\n')
 
     scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_dict(gcp_secret, scope)
