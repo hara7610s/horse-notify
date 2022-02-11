94b692eed41dbcde3ccf41a871de94486845e8f0
hara7610s <hara7610s@gmail.com>
2022-02-11 19:41:52 +0900

    debug

--- checking_entry.py
+++ checking_entry.py
@@ -8,7 +8,10 @@ def notify_entry(entry_dict, my_dict):
             RaceNum = entry_dict[my_horse][1].decode('utf-8')
             RaceName = entry_dict[my_horse][2].decode('utf-8')
             HorseName = my_horse.decode('utf-8')
-            Comment = my_dict[my_horse].decode('utf-8')
+            comments = my_dict[my_horse]
+            text = ""
+            for comment in comments:
+                text = text + "\n" + comment.decode('utf-8')
             
-            line.notify_message(f'\n{RaceCourse}{RaceNum} {RaceName}\n{HorseName}\n{Comment}')
+            line.notify_message(f'\n{RaceCourse}{RaceNum} {RaceName}\n{HorseName}{text}')
             time.sleep(1)
