3e617e7469c20a3c39eae88773ba14c5c70ee8cd
hara7610s <hara7610s@gmail.com>
2022-02-11 14:53:40 +0900

    debug

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,15 +2,16 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 def read_spreadsheet():
-
+    # authorize gspread
     scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name("gcp_secret.json", scope)
     client = gspread.authorize(creds)
 
+    # get all values as list of lists
     sheet = client.open("horse_DB").sheet1
-    #get all values as list of lists
     mylists = sheet.get_all_values()
     
+    # make dictionary with encoding
     mylist_utf8 =[]
     for mylist in mylists:
         mylist_utf8.append(list(map(lambda s:s.encode("utf-8"), mylist)))
