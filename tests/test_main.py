c6f6f4f0b6a470345613a3755e0d0b2c75cd0609
hara7610s <hara7610s@gmail.com>
2024-05-28 21:27:18 +0900

    update test condition

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,6 +9,7 @@ from checking_entry import notify_entry
 
 def test_main():
     entry_dict = make_entry_dict(race_id="201706050811")
+    print(entry_dict)
     my_dict = {"キタサンブラック": ""}
 
     notify_entry(entry_dict=entry_dict, my_dict=my_dict)
