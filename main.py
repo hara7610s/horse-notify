1464242b50b7fdf042db976293c8e531474f411d
hara7610s <hara7610s@gmail.com>
2024-05-28 23:01:29 +0900

    change directory structure

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import line
-import find_race
-import find_horse
 import datetime
-import spreadsheet
+
 import checking_entry
+from scraping_utils import spreadsheet, get_race_list, find_horses
 
 def main():
     # find race pages tomorrow
     tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    race_id_list = find_race.get_race_list(tomorrow)
+    race_id_list = get_race_list(tomorrow)
     
     # make a list of horses entered tomorrow
     if len(race_id_list) == 0:
@@ -16,7 +15,7 @@ def main():
     else:
         entry_dict = {}
         for race_id in race_id_list:
-            each_race = find_horse.make_entry_dict(race_id)
+            each_race = find_horses(race_id)
 
             entry_dict.update(each_race)
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 sys.path.append(Path.cwd().parent)
 
-from main import main
-from find_horse import make_entry_dict
-from checking_entry import notify_entry
+from scraping_utils import find_horses
+from messaging_utils import search_and_notify_entry
 
 def test_main():
-    entry_dict = make_entry_dict(race_id="201706050811")
-    print(entry_dict)
+    entry_dict = find_horses(race_id="201706050811")
     my_dict = {"キタサンブラック".encode("utf-8"): ""}
 
-    notify_entry(entry_dict=entry_dict, my_dict=my_dict)
+    search_and_notify_entry(entry_dict=entry_dict, my_dict=my_dict)
